0e3cb6a77043ca1c09741ec978636723f9a5c983
lionelsu <lionel_suicune@hotmail.com>
2022-07-24 08:53:53 -0400

    update class

--- app-soccer-news/app/src/main/java/me/dio/soccernews/ui/adapters/NewsAdapter.java
+++ app-soccer-news/app/src/main/java/me/dio/soccernews/ui/adapters/NewsAdapter.java
@@ -1,22 +1,38 @@
 package me.dio.soccernews.ui.adapters;
 
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.ObjectInputStream;
+import java.util.List;
+
 import me.dio.soccernews.databinding.NewsItemBinding;
+import me.dio.soccernews.domain.News;
 
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
+
+    private List<News> news;
+
+    public NewsAdapter(List<News> news) {
+        this.news = news;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        NewsItemBinding binding = NewsItemBinding.inflate(layoutInflater, parent, false);
+        return new ViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        News news = this.news.get(position);
+        holder.binding.tvTitle.setText(news.getTitle());
+        holder.binding.tvDescription.setText(news.getDescription());
 
     }
 
--- app-soccer-news/app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
+++ app-soccer-news/app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
@@ -1,31 +1,64 @@
 package me.dio.soccernews.ui.news;
 
+import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import me.dio.soccernews.data.SoccerNewsRepository;
 import me.dio.soccernews.domain.News;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class NewsViewModel extends ViewModel {
 
-    private final MutableLiveData<List<News>> news;
+    public enum State {
+        DOING, DONE, ERROR;
+    }
+
+    private final MutableLiveData<List<News>> news = new MutableLiveData<>();
+    private final MutableLiveData<State> state = new MutableLiveData<>();
 
     public NewsViewModel() {
-        this.news = new MutableLiveData<>();
+        this.findNews();
+    }
 
-        ///TODO Remover Mock de Notícias
-        List<News> news = new ArrayList<>();
-        news.add(new News(title "Ferroviária Tem Desfalque Importante", description "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."));
-        news.add(new News(title "Ferrinha Joga Sábado ", description "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."));
-        news.add(new News(title "Copa do Mundo Feminina Está Terminando", description "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."));
+    public void findNews() {
+        state.setValue(State.DOING);
+        SoccerNewsRepository.getInstance().getRemoteApi().getNews().enqueue(new Callback<List<News>>() {
+            @Override
+            public void onResponse(@NonNull Call<List<News>> call, @NonNull Response<List<News>> response) {
+                if (response.isSuccessful()) {
+                    news.setValue(response.body());
+                    state.setValue(State.DONE);
+                } else {
+                    state.setValue(State.ERROR);
+                }
+            }
 
-        this.news.setValue(news);
+            @Override
+            public void onFailure(@NonNull Call<List<News>> call, Throwable error) {
+                //FIXME Tirar o printStackTrace quando formos para produção!
+                error.printStackTrace();
+                state.setValue(State.ERROR);
+            }
+        });
+    }
+
+    public void saveNews(News news) {
+        AsyncTask.execute(() -> SoccerNewsRepository.getInstance().getLocalDb().newsDao().save(news));
     }
 
     public LiveData<List<News>> getNews() {
-        return news;
+        return this.news;
+    }
+
+    public LiveData<State> getState() {
+        return this.state;
     }
 }
